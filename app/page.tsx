bbde9b05e8e05770d67350c786ab6f7a863c4c4c
Maximus <hey@m4xy.org>
2024-09-21 00:35:57 +0530

    site work pls

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div
+      className="flex flex-col justify-center items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-no-repeat bg-cover"
+    >
+      <h1 className="font-extrabold text-8xl bg-clip-text custom-clip">COMING SOON.</h1>
+    </div>
+  );
+}
